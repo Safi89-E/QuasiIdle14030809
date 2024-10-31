8a3e2244bc964c4cc709ac6476c93de068e7b686
Safi89-E <tafakor.safi@gmail.com>
2024-10-31 19:06:25 +0330

    Update QuasiIdle.py
    add Error color & import "SaveAs"

--- QuasiIdle.py
+++ QuasiIdle.py
@@ -1,7 +1,7 @@
 from SaveAsClass import SaveAsWindow
 import subprocess
 import os
-from SaveAsClass import  read_file_content
+
 from PyQt6.QtWidgets import (  
     QApplication,  
     QMainWindow,  
@@ -10,14 +10,16 @@ from PyQt6.QtWidgets import (
     QHBoxLayout,
     QPlainTextEdit  
 )  
+
 from PyQt6.QtGui import  QAction, QColor 
+from SaveAsClass import SaveAsWindow
       
 class MainWindow(QMainWindow):  
     def __init__(self):  
         super().__init__()
         self.setWindowTitle("littleIDLE")
        
-        self.setGeometry(100, 100, 800, 800)  
+        self.setGeometry(100, 100, 600, 600)  
 
        
         central_widget = QWidget()  
@@ -25,30 +27,33 @@ class MainWindow(QMainWindow):
 
       
         layout = QHBoxLayout()  
-        central_widget.setLayout(layout)  
+        central_widget.setLayout(layout) 
+        
 
-        self.code_edit = QTextEdit(self)  
-        self.code_edit.setPlaceholderText("Write your Python code here...")
-        line_count = self.code_edit.blockCount()
-        layout.addWidget(self.code_edit)
-
-        line_number = ""
-        for i in range(1,line_count+1):
-            line_number += "i\n"
         self.line_number = QTextEdit(self) 
-        self.line_number.setFixedSize(20, 800) 
-        self.line_number.setPlaceholderText(line_number)
+        self.line_number.setFixedSize(25, 600)
+        line_num = ''
+        for i in range(1,40):
+            line_num += str(i)+"\n"
+        self.line_number.setPlaceholderText(line_num)
         self.line_number.setReadOnly(True)
         layout.addWidget(self.line_number)
+
+        self.code_edit = QTextEdit(self)  
+        self.code_edit.setStyleSheet("QTextEdit { background-color: blue; color: white; }")
+        self.code_edit.setPlaceholderText("Write your Python code here...")
+        if "print" in self.code_edit.toPlainText():
+            self.code_edit.setTextColor(QColor(128, 0, 128))
+  
+        layout.addWidget(self.code_edit)
        
-        self.output_edit = QTextEdit()  
+        self.output_edit = QTextEdit()
+        self.output_edit_fake = QTextEdit()  
         self.output_edit.setReadOnly(True)  
         layout.addWidget(self.output_edit) 
 
-        if "Error" in self.output_edit.toPlainText():
-             self.output_edit.selectAll()  
-             self.output_edit.setTextColor(QColor("red")) 
-             self.output_editt.setCurrentFont(self.output_edit.font()) 
+       
+             
 
         
         menu_bar = self.menuBar()  
@@ -114,14 +119,26 @@ class MainWindow(QMainWindow):
         run_action = QAction("Run Module", self)   
         run_action.triggered.connect(self.run_file)   
         run_menu.addAction(run_action) 
-
     def run_file(self):  
         script = self.code_edit.toPlainText()
         with open("empty.py", "w") as f:
            f.write(script)
 
-        p01 = subprocess.getoutput(["python", "empty.py"])
-        self.output_edit.setText(p01)
+        Shell_txt = subprocess.getoutput(["python", "empty.py"])
+        self.output_edit_fake.setText(Shell_txt)
+        self.output_edit.selectAll()
+
+        if "Error" in self.output_edit_fake.toPlainText():
+              
+            self.output_edit.setTextColor(QColor("red")) 
+        
+        else:
+           
+            self.output_edit.setTextColor(QColor("blue")) 
+
+        self.output_edit.setText(Shell_txt)    
+     
+       
     
     def save_file(self):
         counter = 1
@@ -139,8 +156,11 @@ class MainWindow(QMainWindow):
          
        
     def saveAs_file(self):
+
+        self.save_as_window = SaveAsWindow()
+        self.save_as_window.show()
                        
-        subprocess.getoutput(["python", "SaveAsClass.py"])
+        #subprocess.getoutput(["python", "SaveAsClass.py"])
     
     def find_text(self):  
          
@@ -148,7 +168,17 @@ class MainWindow(QMainWindow):
 
     def replace_text(self):  
         print("Replace action triggered")  
-
+'''
+class MyTextEdit(QTextEdit):
+        def keyPressEvent(self, event):
+            if event.key() == 16777220:  # کلید اینتر
+                self.line_number.append("2")  # اضافه کردن عدد 2 به خط شماره
+            self.line_number = MyTextEdit(self) 
+            self.line_number.setFixedSize(20, 800) 
+            self.line_number.setPlaceholderText("1\n2")
+            self.line_number.setReadOnly(True)'''
+               
+        
 if __name__ == "__main__":  
     app = QApplication([])  
     window = MainWindow()  
